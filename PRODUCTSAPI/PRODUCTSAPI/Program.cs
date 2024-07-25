e329c2099dfa9b1f2494456809a2fc0e91a5837b
JAIR GÓMEZ <jair.gomez@improve-it.com.ec>
2024-07-25 09:48:15 -0500

    works with default database model and create table from scratch

--- PRODUCTSAPI/PRODUCTSAPI/Program.cs
+++ PRODUCTSAPI/PRODUCTSAPI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using PRODUCTSAPI.Data;
 
 
@@ -10,6 +11,20 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        context.Database.EnsureCreated(); // Crea la base de datos si no existe
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB.");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
